108178afaf17a5951f99fbde38e3c8f60b7fa994
maryaposohova <maryaposohova1202@gmail.com>
2024-08-03 23:02:42 +0300

    Add files via upload

---
+++ Дз1урока.py
@@ -0,0 +1,15 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Автомобиль'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[0::2])
+
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[0::2])
