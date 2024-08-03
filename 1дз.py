84b24186edf9487438645b540586c761ec7cd13f
maryaposohova <maryaposohova1202@gmail.com>
2024-08-03 21:30:26 +0300

    Add files via upload

---
+++ 1дз.py
@@ -0,0 +1,13 @@
+#1st programm
+print(9**0.5*5)
+
+#2nd programm
+print(9.99>9.98 and 1000!=1001)
+
+#3rd programm
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#4th programm
+print(int(float('123.456') * 10)% 10)
