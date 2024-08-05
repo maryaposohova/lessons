f3539bdcaa456f86fc9407ff7365aa61488b99a2
maryaposohova <maryaposohova1202@gmail.com>
2024-08-05 11:46:13 +0300

    Add files via upload

--- Дз2урока.py
+++ Дз2урока.py
@@ -34,5 +34,13 @@ name_course ='Python'
 time_at_1_homework =1.5/12
 print ('Курс:',name_course,','.lstrip())
 
+#Практическое задание по работе в Pycharm - "Переменные"
+
+how_many_homework =12
+hours =1.5
+name_course ='Python'
+time_at_1_homework =1.5/12
+print ('Курс: ',name_course,',', ' всего задач: ', how_many_homework, ',', ' затрачено часов: ',
+       hours, ',', ' среднее время выполнения: ',time_at_1_homework,' часа.', sep='')
 
 
