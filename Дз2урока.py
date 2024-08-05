5a21ce30fa740c26fb15d96fface36c61d6bb469
maryaposohova <maryaposohova1202@gmail.com>
2024-08-05 11:15:14 +0300

    Add files via upload

---
+++ Дз2урока.py
@@ -0,0 +1,38 @@
+#Практическое задание по работе в Pycharm - "Переменные"
+
+how_many_homework =12
+hours =1.5
+name_course ='Python'
+time_at_1_homework =1.5/12
+print ('Курс:',name_course,',', 'всего задач:', how_many_homework, ',', 'затрачено часов:',
+       hours, ',', 'среднее всемя выполнения:',time_at_1_homework,'часа.')
+
+
+
+#Практическое задание по работе в Pycharm - "Переменные"
+
+how_many_homework =12
+hours =1.5
+name_course ='Python'
+time_at_1_homework =1.5/12
+print ('Курс:',name_course.rstrip(),',')
+
+
+#Практическое задание по работе в Pycharm - "Переменные"
+
+how_many_homework =12
+hours =1.5
+name_course ='Python'.rstrip()
+time_at_1_homework =1.5/12
+print ('Курс:',name_course,',')
+
+#Практическое задание по работе в Pycharm - "Переменные"
+
+how_many_homework =12
+hours =1.5
+name_course ='Python'
+time_at_1_homework =1.5/12
+print ('Курс:',name_course,','.lstrip())
+
+
+
