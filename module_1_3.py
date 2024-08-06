ebb846f5b676c7643e1340247b97585072c4b296
maryaposohova <maryaposohova1202@gmail.com>
2024-08-06 18:36:08 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+#Практическая работа по уроку "Динамическая типизация"
+
+name = 'Marya'
+print('Name: '+ name)
+age = 44
+print('Age:', age)
+age = age+1
+print('New age:', age)
+# is_student= age<age+1
+is_student= age==age
+print('Is_student:', is_student)
