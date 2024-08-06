62208dd3cbf60b06f5aed2b54085826ce08c0357
maryaposohova <maryaposohova1202@gmail.com>
2024-08-06 18:57:42 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -9,3 +9,16 @@ print('New age:', age)
 # is_student= age<age+1
 is_student= age==age
 print('Is_student:', is_student)
+
+
+name = 'Marya'
+print('Name: '+ name)
+age = 44
+print('Age:', age)
+age = age+1
+new_age = 45
+print('New age:', age)
+# is_student= age<age+1
+is_student= new_age==age
+print('Is_student:', is_student)
+
